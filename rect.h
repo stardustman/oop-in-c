7488722a8ddb693c9ed74b130a2628d000d42b9e
stardust <aoyunyoung@gmail.com>
2019-03-22 16:37:27 +0800

    add rectangle.h

---
+++ rect.h
@@ -0,0 +1,17 @@
+#ifndef RECT_H_INCLUDED
+#define RECT_H_INCLUDED
+/* Rectangle's attributes...*/
+
+typedef struct {
+    Shape super; /* <== inherits Shape*/
+
+    /* attributes added by this subclass...*/
+     unsigned int width;
+     unsigned int height;
+} Rectangle;
+
+/* constructor prototype*/
+void Rectangle_ctor(Rectangle * const me,int x,int y ,unsigned int width,unsigned int height);
+
+
+#endif // RECT_H_INCLUDED
